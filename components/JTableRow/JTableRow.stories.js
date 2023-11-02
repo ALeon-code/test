5b54dbf492d1a91e113ae0daf6724ae11b20b069
AlianLeon <alian.elbor@gmail.com>
2023-11-02 10:04:55 -0400

    base

---
+++ components/JTableRow/JTableRow.stories.js
@@ -0,0 +1,17 @@
+
+import JTableRow from './JTableRow.vue'
+
+export default {
+    title: 'Components/JTableRow',
+    component: JTableRow,
+    tags: ['autodocs'],
+}
+
+export const Base = {
+    args: {
+        values: [
+            'José Silva',
+            'email@email.com'
+        ]
+    },
+};
---
+++ components/JTableRow/JTableRow.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {ref, reactive, computed} from 'vue';
+
+const props = defineProps({
+  values: {
+    type: Array,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <tr>
+    <td v-for="(value, index) in values" :key="index">
+      {{ value }}
+    </td>
+  </tr>
+</template>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+tr {
+  background-color: white;
+  border-radius: 4px;
+  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.08);
+}
+td {
+  padding: 8px;
+}
+</style>
